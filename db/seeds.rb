104383de25864affbdf5fc5ac2c679cfadd24b06
Andrew Taber <andrew.e.taber@gmail.com>
2014-01-07 13:34:27 -0800

    Modify seeds, don't show description if there is none

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ user = User.create(:email => "andrew.e.taber@gmail.com", :password => "nutpalace
 user2 = User.create(:email => "jesus.shuttlesworth@gmail.com", :password => "rayallenfor3", :password_confirmation => "rayallenfor3")
 user3 = User.create(:email => "king.james@gmail.com", :password => "fuckyoukobe", :password_confirmation => "fuckyoukobe")
 Performer.create(:name => "Pink Floyd", :genre => "Rock", :user_id => user.id)
-Performer.create(:name => "Chance the Rapper", :genre => "Rock", :user_id => user.id, :soundcloud_url => "https://soundcloud.com/chancetherapper/sets/chance-the-rapper-acid-rap")
+Performer.create(:name => "Chance the Rapper", :genre => "Rock", :user_id => user.id, :soundcloud_url => "https://soundcloud.com/chancetherapper/sets/chance-the-rapper-acid-rap", :description => "A raunchy time from a pothead with too much of it")
 Performer.create(:name => "Skrillex", :genre => "Electric", :user_id => user2.id)
