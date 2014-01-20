8caf4f842475348fbceb1d72aa2ccf1112ba7e86
Andrew Taber <andrew.e.taber@gmail.com>
2014-01-19 23:09:59 -0800

    Clean up booking controller with strong params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,28 @@
 class BookingsController < ApplicationController
-  before_filter :stripe_credentials
+  before_filter :stripe_credentials, :except => :index
+  before_filter :find_performer
+
+  def index
+    @bookings = @performer.bookings.sort(&:created_at)
+  end
 
   def create
     # save the stripe details for later, when the performer accepts the booking
-    token = params[:stripeToken]
+    token = params.require(:stripeToken)
     customer = Stripe::Customer.create(
       :card => token,
-      :description => params[:stripeEmail]
+      :description => params.require(:stripeEmail)
       )
-    save_stripe_customer_id(user, customer.id)
+    save_stripe_customer_id(current_user, customer.id)
 
-    @booking   = Booking.new(params[:booking])
-    @performer = Performer.find(params[:performer_id])
+    @booking = Booking.new(params.require(:booking).permit(:event_date, :cost))
+    if params[:comment].present?
+      comment = @booking.comments.build(:content => params[:comment])
+      comment.user = current_user
+      comment.save
+    end
     @booking.performer = @performer
+
     if @booking.save
       flash[:notice] = "Your booking has been created, check your profile to see if the performer has accepted."
     else
@@ -22,15 +32,34 @@ class BookingsController < ApplicationController
   end
 
   def accept
-    customer_id = get_stripe_customer_id(user)
-    Stripe::Charge.create(
-      :amount => cost,
-      :currency => "usd",
-      :customer => customer_id
-      )
+    if customer_id = get_stripe_customer_id(current_user)
+      Stripe::Charge.create(
+        :amount => cost,
+        :currency => "usd",
+        :customer => customer_id
+        )
+    else
+      flash[:error] = "The user that requested this booking doesn't have any payment info associated"
+    end
   end
 
   private
+  def find_performer
+    @performer ||= Performer.find(params.require(:performer_id))
+  end
+
+  def save_stripe_customer_id(user, id)
+    user.stripe_customer_id = id
+    user.save!
+  end
+
+  def get_stripe_customer_id(user)
+    if user.stripe_customer_id.present?
+      return user.stripe_customer_id
+    else
+      throw "No Payment Data For This User"
+    end
+  end
 
   def stripe_credentials
     Stripe.api_key = ENV["STRIPE_API_KEY"]
