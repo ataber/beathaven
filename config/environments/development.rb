979543cb69c97194e2283ee7f749a4e52b540e83
Andrew Taber <andrew.e.taber@gmail.com>
2014-03-09 14:41:23 -0700

    Separate billing form from rest of performer form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,12 +20,12 @@ Beathaven::Application.configure do
   Stripe.api_key = ENV["STRIPE_API_KEY"]
 
   config.paperclip_defaults = {
-    :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_protocol => "http"
   }
 
   # Don't care if the mailer can't send.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 user = User.create(:email => "andrew.e.taber@gmail.com", :password => "nutpalace", :password_confirmation => "nutpalace")
 user2 = User.create(:email => "jesus.shuttlesworth@gmail.com", :password => "rayallenfor3", :password_confirmation => "rayallenfor3")
-user3 = User.create(:email => "king.james@gmail.com", :password => "fuckyoukobe", :password_confirmation => "fuckyoukobe")
+User.create(:email => "king.james@gmail.com", :password => "fuckyoukobe", :password_confirmation => "fuckyoukobe")
 Performer.create(:name => "Pink Floyd", :genre => "Rock", :user_id => user.id, :price => 50)
-Performer.create(:name => "Chance the Rapper", :genre => "Rock", :user_id => user.id, :soundcloud_url => "https://soundcloud.com/chancetherapper/sets/chance-the-rapper-acid-rap", :description => "A raunchy time from a pothead with too much of it", :price => 200)
+chance = Performer.create(:name => "Chance the Rapper", :genre => "Rock", :user_id => user.id, :soundcloud_url => "https://soundcloud.com/chancetherapper/sets/chance-the-rapper-acid-rap", :description => "A raunchy time from a pothead with too much of it", :price => 200)
+file = File.open(Rails.root.join("app/assets/images/chance.jpg"))
+chance.avatar = file
+file.close()
+chance.save!
 Performer.create(:name => "Skrillex", :genre => "Electric", :user_id => user2.id, :price => 20)
