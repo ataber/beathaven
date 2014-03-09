447b01a42673e6cebbc014275e78c7c64425a338
Andrew Taber <andrew.e.taber@gmail.com>
2014-03-09 16:23:49 -0700

    Use s3 for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Beathaven::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   config.paperclip_defaults = {
+    :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
